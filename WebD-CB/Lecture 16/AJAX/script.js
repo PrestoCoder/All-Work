e986fc6460701082b3424d176b7823441735f92c
ROHAN CHHIBBA <rohanchhibba@ROHANs-MacBook-Pro.local>
2023-04-10 23:19:33 +0530

    ToDoList using Jquery, self-understanding xhr, fetch, and axios

--- WebD-CB/Lecture 16/AJAX/script.js
+++ WebD-CB/Lecture 16/AJAX/script.js
@@ -4,4 +4,46 @@
 
 // 2 methods
 
-// Both on Github
+// AJAX: Asynchronous JavaScript and XML
+// XML? Puratan waqt mein data XML format mein bheja jaata tha, jo HTML se bhot similar tha.
+// Par aaj ke samay mein data JSON mein bheja jaata hai.
+// Par parampara ko banaye rakhne ke liye, 
+// aur purana code break na ho isliye, XMLHttpRequest ka naam JSONHttpRequest nahin rakha gaya.
+const URL = 'https://cat-fact.herokuapp.com/facts';
+const xhr = new XMLHttpRequest();
+const btn = document.querySelector('.btn');
+const factsList = document.querySelector('.factsList');
+
+// Request kaha aur kis type ki bhejni hai define karo
+xhr.open("GET",URL);
+
+// Request ko send kab krna hai?
+btn.addEventListener('click',()=>{
+    xhr.send();
+})
+
+// Request ke success par yeh chlega
+xhr.onload = function(data){
+    console.log(data);
+    console.log(data.currentTarget.response);
+    // Response JSON format mein hai, tabhi usko parse karke JSON object banana padhega.
+    const res = JSON.parse(data.currentTarget.response);
+    const factsData = res.map((d)=>d.text);
+    console.log(factsData)
+
+    addDataToList(factsData);
+    // const data = res.text;
+}
+
+// Request ke fail hone par yeh chlega
+xhr.onerror = function(){
+    console.log("Lagg gai");
+}
+
+function addDataToList(data) {
+    data.forEach((d) => {
+        let li = document.createElement('li');
+        li.innerText = d;
+        factsList.appendChild(li);
+    })
+}
