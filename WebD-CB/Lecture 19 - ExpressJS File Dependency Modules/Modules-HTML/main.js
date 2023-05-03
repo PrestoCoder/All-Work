9a7d60d6ec76e24ee884d6d8448d6b2bff636d95
Rohan Chhibba <rohanchhibba96@gmail.com>
2023-05-03 18:06:02 +0530

    Renaming folders

--- WebD-CB/Lecture 19 - ExpressJS File Dependency Modules/Modules-HTML/main.js
+++
@@ -1,2 +0,0 @@
-console.log(add(1,2));
-console.log(subtract(10,5));
--- WebD-CB/Lecture 19 - ExpressJS File Dependency, Modules/intro2express/script.js
+++ WebD-CB/Lecture 19 - ExpressJS File Dependency, Modules/intro2express/script.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 4444;
 
+// By default, this is a get request.
+// Replacing with post won't work
+// https://stackoverflow.com/questions/56129326/do-browser-send-page-request-as-get-http-method-for-all-page-requests
 app.get('/',(req,res)=>{
     // console.log(req);
     res.send("Hello Welcome to my APP");
