d6a22bfd96af329697bdfabae009268b42e09fbc
Rohan Chhibba <rohanchhibba96@gmail.com>
2023-05-06 16:53:27 +0530

    Using DB with JS

---
+++ WebD-CB/Lecture_27_2/app.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 4444;
+
+app.set('view engine', 'hbs');
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "")));
+
+
+
+app.listen(PORT, () => {
+    console.log(`connected to server ${PORT}`);
+})
